486e2e0b80551af99ee48ab83a55c12e95d46aa5
Brendon Smith <75303634+BrendonSmith8888@users.noreply.github.com>
2023-02-14 23:49:04 +0200

    Updated Tables and added new Images

--- javascripts/script.js
+++ javascripts/script.js
@@ -8,13 +8,14 @@ function filterTable() {
   for (i = 0; i < tr.length; i++) {
     td = tr[i].getElementsByTagName("td")[0];
     td1 = tr[i].getElementsByTagName("td")[1];
-    td2 = tr[i].getElementsByTagName("td")[2];
-    td3 = tr[i].getElementsByTagName("td")[10];
-    td4 = tr[i].getElementsByTagName("td")[11];
-    td5 = tr[i].getElementsByTagName("td")[17];
-    td6 = tr[i].getElementsByTagName("td")[18];
-    td7 = tr[i].getElementsByTagName("td")[24];
-    td8 = tr[i].getElementsByTagName("td")[25];
+    td2 = tr[i].getElementsByTagName("td")[3];
+    td3 = tr[i].getElementsByTagName("td")[11];
+    td4 = tr[i].getElementsByTagName("td")[13];
+    td5 = tr[i].getElementsByTagName("td")[19];
+    td6 = tr[i].getElementsByTagName("td")[21];
+    td7 = tr[i].getElementsByTagName("td")[27];
+    td8 = tr[i].getElementsByTagName("td")[29];
+
     if (td) {
       if (
         td.innerHTML.toUpperCase().indexOf(filter) > -1 ||
@@ -45,14 +46,14 @@ function filterUnit() {
   for (i = 0; i < tr.length; i++) {
     td = tr[i].getElementsByTagName("td")[0];
     td1 = tr[i].getElementsByTagName("td")[1];
-    td2 = tr[i].getElementsByTagName("td")[2];
-    td3 = tr[i].getElementsByTagName("td")[13];
-    td4 = tr[i].getElementsByTagName("td")[14];
-    td5 = tr[i].getElementsByTagName("td")[23];
-    td6 = tr[i].getElementsByTagName("td")[24];
-    td7 = tr[i].getElementsByTagName("td")[33];
-    td8 = tr[i].getElementsByTagName("td")[34];
-    td9 = tr[i].getElementsByTagName("td")[42];
+    td2 = tr[i].getElementsByTagName("td")[3];
+    td3 = tr[i].getElementsByTagName("td")[14];
+    td4 = tr[i].getElementsByTagName("td")[16];
+    td5 = tr[i].getElementsByTagName("td")[25];
+    td6 = tr[i].getElementsByTagName("td")[27];
+    td7 = tr[i].getElementsByTagName("td")[36];
+    td8 = tr[i].getElementsByTagName("td")[38];
+    td9 = tr[i].getElementsByTagName("td")[47];
     if (td) {
       if (
         td.innerHTML.toUpperCase().indexOf(filter) > -1 ||
@@ -75,7 +76,7 @@ function filterUnit() {
 }
 
 function filterTechnology() {
-  let input, filter, table, tr, td, td1, td2, td3, i;
+  let input, filter, table, tr, td, td1, td2, td3, td4, td5, td6, td7, i;
   input = document.getElementById("userTechnologyInput");
   filter = input.value.toUpperCase();
   table = document.getElementById("aoe1technologyTable");
@@ -84,14 +85,22 @@ function filterTechnology() {
   for (i = 0; i < tr.length; i++) {
     td = tr[i].getElementsByTagName("td")[0];
     td1 = tr[i].getElementsByTagName("td")[1];
-    td2 = tr[i].getElementsByTagName("td")[3];
-    td3 = tr[i].getElementsByTagName("td")[4];
+    td2 = tr[i].getElementsByTagName("td")[2];
+    td3 = tr[i].getElementsByTagName("td")[3];
+    td4 = tr[i].getElementsByTagName("td")[4];
+    td5 = tr[i].getElementsByTagName("td")[5];
+    td6 = tr[i].getElementsByTagName("td")[7];
+    td7 = tr[i].getElementsByTagName("td")[8];
     if (td) {
       if (
         td.innerHTML.toUpperCase().indexOf(filter) > -1 ||
         td1.innerHTML.toUpperCase().indexOf(filter) > -1 ||
         td2.innerHTML.toUpperCase().indexOf(filter) > -1 ||
-        td3.innerHTML.toUpperCase().indexOf(filter) > -1
+        td3.innerHTML.toUpperCase().indexOf(filter) > -1 ||
+        td4.innerHTML.toUpperCase().indexOf(filter) > -1 ||
+        td5.innerHTML.toUpperCase().indexOf(filter) > -1 ||
+        td6.innerHTML.toUpperCase().indexOf(filter) > -1 ||
+        td7.innerHTML.toUpperCase().indexOf(filter) > -1 
       ) {
         tr[i].style.display = "";
       } else {
