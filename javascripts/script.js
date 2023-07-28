503ad5a2da5eab2824619c935b6620d861af20d6
Brendon Smith <75303634+BrendonSmith8888@users.noreply.github.com>
2023-07-28 22:39:49 +0100

    Update

--- javascripts/script.js
+++ javascripts/script.js
@@ -1,35 +1,78 @@
-function filterTable() {
-  let input, filter, table, tr, td, td1, td2, td3, td4, td5, td6, td7, td8, i;
-  input = document.getElementById("userBuildingInput");
+function filterCivilization() {
+  let input, filter, table, tr, td, i;
+  input = document.getElementById("userCivilisationInput");
   filter = input.value.toUpperCase();
-  table = document.getElementById("buildingsTable");
+  table = document.getElementById("aoebuildingsTable");
   tr = table.getElementsByTagName("tr");
 
   for (i = 0; i < tr.length; i++) {
     td = tr[i].getElementsByTagName("td")[0];
-    td1 = tr[i].getElementsByTagName("td")[1];
-    td2 = tr[i].getElementsByTagName("td")[3];
-    td3 = tr[i].getElementsByTagName("td")[11];
-    td4 = tr[i].getElementsByTagName("td")[13];
-    td5 = tr[i].getElementsByTagName("td")[19];
-    td6 = tr[i].getElementsByTagName("td")[21];
-    td7 = tr[i].getElementsByTagName("td")[27];
-    td8 = tr[i].getElementsByTagName("td")[29];
-
     if (td) {
-      if (
-        td.innerHTML.toUpperCase().indexOf(filter) > -1 ||
-        td1.innerHTML.toUpperCase().indexOf(filter) > -1 ||
-        td2.innerHTML.toUpperCase().indexOf(filter) > -1 ||
-        td3.innerHTML.toUpperCase().indexOf(filter) > -1 ||
-        td4.innerHTML.toUpperCase().indexOf(filter) > -1 ||
-        td5.innerHTML.toUpperCase().indexOf(filter) > -1 ||
-        td6.innerHTML.toUpperCase().indexOf(filter) > -1 ||
-        td7.innerHTML.toUpperCase().indexOf(filter) > -1 ||
-        td8.innerHTML.toUpperCase().indexOf(filter) > -1
-      ) {
+      if (td.innerHTML.toUpperCase().indexOf(filter) > -1) {
         tr[i].style.display = "";
-      } else {
+      }
+      else {
+        tr[i].style.display = "none";
+      }
+    }
+  }
+}
+
+function filterBuildings() {
+  let input, filter, table, tr, td, i;
+  input = document.getElementById("userBuildingInput");
+  filter = input.value.toUpperCase();
+  table = document.getElementById("aoebuildingsTable");
+  tr = table.getElementsByTagName("tr");
+
+  for (i = 0; i < tr.length; i++) {
+    td = tr[i].getElementsByTagName("td")[1];
+    if (td) {
+      if (td.innerHTML.toUpperCase().indexOf(filter) > -1) {
+        tr[i].style.display = "";
+      }
+      else {
+        tr[i].style.display = "none";
+      }
+    }
+  }
+}
+
+function filterAge() {
+  let input, filter, table, tr, td, i;
+  input = document.getElementById("userAgeInput");
+  filter = input.value.toUpperCase();
+  table = document.getElementById("aoebuildingsTable");
+  tr = table.getElementsByTagName("tr");
+
+  for (i = 0; i < tr.length; i++) {
+    td = tr[i].getElementsByTagName("td")[3];
+    td1 = tr[i].getElementsByTagName("td")[4];
+    if (td) {
+      if (td.innerHTML.toUpperCase().indexOf(filter) > -1 || td1.innerHTML.toUpperCase().indexOf(filter) > -1) {
+        tr[i].style.display = "";
+      }
+      else {
+        tr[i].style.display = "none";
+      }
+    }
+  }
+}
+
+function filterRequirements() {
+  let input, filter, table, tr, td, i;
+  input = document.getElementById("userRequirementsInput");
+  filter = input.value.toUpperCase();
+  table = document.getElementById("aoebuildingsTable");
+  tr = table.getElementsByTagName("tr");
+
+  for (i = 0; i < tr.length; i++) {
+    td = tr[i].getElementsByTagName("td")[16];
+    if (td) {
+      if (td.innerHTML.toUpperCase().indexOf(filter) > -1) {
+        tr[i].style.display = "";
+      }
+      else {
         tr[i].style.display = "none";
       }
     }
@@ -40,7 +83,7 @@ function filterUnit() {
   let input, filter, table, tr, td, td1, td2, td3, td4, td5, td6, td7, td8, td9, i;
   input = document.getElementById("userUnitInput");
   filter = input.value.toUpperCase();
-  table = document.getElementById("aoe1unitsTable");
+  table = document.getElementById("aoeunitsTable");
   tr = table.getElementsByTagName("tr");
 
   for (i = 0; i < tr.length; i++) {
@@ -79,7 +122,7 @@ function filterTechnology() {
   let input, filter, table, tr, td, td1, td2, td3, td4, td5, td6, td7, i;
   input = document.getElementById("userTechnologyInput");
   filter = input.value.toUpperCase();
-  table = document.getElementById("aoe1technologyTable");
+  table = document.getElementById("aoetechnologyTable");
   tr = table.getElementsByTagName("tr");
 
   for (i = 0; i < tr.length; i++) {
