84647dd81552748cc3e1dc9c3d8b29a5b3092c6b
Brendon Smith <75303634+BrendonSmith8888@users.noreply.github.com>
2023-08-06 13:48:03 +0100

    Update AOE1 Technology Search Buttons

--- javascripts/script.js
+++ javascripts/script.js
@@ -38,6 +38,35 @@ function filterCivilizationUnits() {
   }
 }
 
+function filterCivilizationTechnology() {
+  let input, filter, table, tr, td, td1, td2, td3, td4, i;
+  input = document.getElementById("userCivilizationTechnologyInput");
+  filter = input.value.toUpperCase();
+  table = document.getElementById("aoetechnologyTable");
+  tr = table.getElementsByTagName("tr");
+
+  for (i = 0; i < tr.length; i++) {
+    td = tr[i].getElementsByTagName("td")[0];
+    td1 = tr[i].getElementsByTagName("td")[1];
+    td2 = tr[i].getElementsByTagName("td")[2];
+    td3 = tr[i].getElementsByTagName("td")[3];
+    td4 = tr[i].getElementsByTagName("td")[4];
+    if (td) {
+      if (
+        td.innerHTML.toUpperCase().indexOf(filter) > -1 ||
+        td1.innerHTML.toUpperCase().indexOf(filter) > -1 ||
+        td3.innerHTML.toUpperCase().indexOf(filter) > -1 ||
+        td.innerHTML.toUpperCase().indexOf(filter) > -1 ||
+        td4.innerHTML.toUpperCase().indexOf(filter) > -1
+      ) {
+        tr[i].style.display = "";
+      } else {
+        tr[i].style.display = "none";
+      }
+    }
+  }
+}
+
 function filterBuildings() {
   let input, filter, table, tr, td, i;
   input = document.getElementById("userBuildingInput");
@@ -138,6 +167,26 @@ function aoe1filterAgeUnits() {
   }
 }
 
+function aoe1filterAgeTechnology() {
+  let input, filter, table, tr, td, i;
+  input = document.getElementById("aoe1userAgeTechnologyInput");
+  filter = input.value.toUpperCase();
+  table = document.getElementById("aoetechnologyTable");
+  tr = table.getElementsByTagName("tr");
+
+  for (i = 0; i < tr.length; i++) {
+    td = tr[i].getElementsByTagName("td")[5];
+    if (td) {
+      if (td.innerHTML.toUpperCase().indexOf(filter) > -1) {
+        tr[i].style.display = "";
+      }
+      else {
+        tr[i].style.display = "none";
+      }
+    }
+  }
+}
+
 function filterRequirements() {
   let input, filter, table, tr, td, i;
   input = document.getElementById("userRequirementsInput");
@@ -167,6 +216,26 @@ function aoe1filterRequirementsUnits() {
 
   for (i = 0; i < tr.length; i++) {
     td = tr[i].getElementsByTagName("td")[14];
+    if (td) {
+      if (td.innerHTML.toUpperCase().indexOf(filter) > -1) {
+        tr[i].style.display = "";
+      }
+      else {
+        tr[i].style.display = "none";
+      }
+    }
+  }
+}
+
+function aoe1filterRequirementsTechnology() {
+  let input, filter, table, tr, td, i;
+  input = document.getElementById("aoe1userRequirementsTechnologyInput");
+  filter = input.value.toUpperCase();
+  table = document.getElementById("aoetechnologyTable");
+  tr = table.getElementsByTagName("tr");
+
+  for (i = 0; i < tr.length; i++) {
+    td = tr[i].getElementsByTagName("td")[8];
     if (td) {
       if (td.innerHTML.toUpperCase().indexOf(filter) > -1) {
         tr[i].style.display = "";
@@ -218,7 +287,7 @@ function filterUnit() {
 }
 
 function aoe1filterUnit() {
-  let input, filter, table, tr, td, td1, td2, td3, td4, td5, td6, td7, td8, td9, i;
+  let input, filter, table, tr, td, i;
   input = document.getElementById("aoe1userUnitInput");
   filter = input.value.toUpperCase();
   table = document.getElementById("aoeunitsTable");
@@ -265,6 +334,27 @@ function filterTechnology() {
         td5.innerHTML.toUpperCase().indexOf(filter) > -1 ||
         td6.innerHTML.toUpperCase().indexOf(filter) > -1 ||
         td7.innerHTML.toUpperCase().indexOf(filter) > -1 
+      ) {
+        tr[i].style.display = "";
+      } else {
+        tr[i].style.display = "none";
+      }
+    }
+  }
+}
+
+function aoe1filterTechnology() {
+  let input, filter, table, tr, td, i;
+  input = document.getElementById("aoe1userTechnologyInput");
+  filter = input.value.toUpperCase();
+  table = document.getElementById("aoetechnologyTable");
+  tr = table.getElementsByTagName("tr");
+
+  for (i = 0; i < tr.length; i++) {
+    td = tr[i].getElementsByTagName("td")[7];
+    if (td) {
+      if (
+        td.innerHTML.toUpperCase().indexOf(filter) > -1  
       ) {
         tr[i].style.display = "";
       } else {
